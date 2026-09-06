8b31a27e16d92ba48e5c8bf57921373857fee8fa
sooriyavigneshwaran250081-bit <sooriyavigneshwaran.250081@csbs.ritchennai.edu.in>
2026-09-06 22:52:23 +0530

    Add files via upload

---
+++ EX-1.py
@@ -0,0 +1,17 @@
+class student:
+    
+    def __init__ (self,rollno,name,course):
+        self.roll = rollno
+        self.name = name
+        self.course = course
+    
+    def display(self):  
+        print("my Roll Number is "+str(self.roll))
+        print("my Name is "+str(self.name))
+        print("my Course is "+str(self.course))
+        
+s1 = student(101,"John","AI&DS")
+s1.display()
+    
+s2 = student(102,"Raj","CSBS")
+s2.display()
